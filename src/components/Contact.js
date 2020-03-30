96d7378446a60f8ccbc8e7167aa33d0a52e9660d
Lenasim <radical.rin@gmail.com>
2020-03-30 21:54:02 +0200

    div structure changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,18 @@ import React from "react"
 import './Contact.css';
 
 
-const Contact = ({ name, avatar, online, status }) => {
-
-
-
+const Contact = ({ name, avatar, online }) => {
     return (
         <div className="Contact">
             <img className="avatar" src={avatar} />
+            <div>
             <div className="name">{name}</div>
 
             <div className="status">
                 <div className={`status-${online ? 'online' : 'offline'}`}></div>
                 <div className="status-text">{online ? 'online' : 'offline'}</div>
             </div>
-
+            </div>
         </div >
     )
 }
